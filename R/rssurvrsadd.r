1dd85667e654bc868c425d49510472c02c85f0cd
Maja Pohar Perme <maja.pohar@mf.uni-lj.si>
2011-01-12 09:00:20 +0000

    version 1.6.2

--- R/rssurvrsadd.r
+++ R/rssurvrsadd.r
@@ -1,41 +1,33 @@
 rs.surv.rsadd <- 
-function (formula, newdata=formula$data, se.fit = TRUE, conf.int = 0.95, individual = FALSE, 
-    vartype, conf.type = c("log", "log-log", "plain", "none"), 
-    call = match.call()) 
+function (formula, newdata) 
 {
    
     call <- match.call()
-    Terms <- terms(formula)  #to rabis, ce je model mal bl smotan - as.factor ali splines ali svasta
+    Terms <- terms(formula$formula)  #to rabis, ce je model mal bl smotan - as.factor ali splines ali svasta
     Terms <- delete.response(Terms)
     newdata <- model.frame(Terms,newdata)
-    resp <- list(y=formula$y,x=newdata)
     n <- formula$n
+    if(formula$method=="max.lik"){
+   	 nvar <- length(formula$coef) - length(formula$int)+1
+    	 formula$coef <- formula$coef[1:nvar]
+    }
     nvar <- length(formula$coef)
     nx <- nrow(newdata)
     nt <- length(formula$times)
     temp <- list(n=formula$n,time=formula$times,call=call,type="right")
     Lambda0 <- formula$Lambda0
-    Lambda0 <- matrix(Lambda0,ncol=nt,nrow=nrow(newdata),byrow=TRUE)
+    Lambda0 <- matrix(Lambda0,ncol=nt,nrow=nx,byrow=TRUE)
     rate <- attr(Terms, "specials")$ratetable
     R <- as.matrix(newdata[, rate,drop=FALSE])
     rat <- attributes(formula$ratetable)$dimid
     mein <- attributes(newdata[,rate])$dimnames[[2]]
     x <- match(rat,mein)
     R <- R[,x,drop=FALSE]
-    newdata <- newdata[,1:(rate-1),drop=FALSE]
-    if(any(formula$mvalue)>0)newdata <- newdata - matrix(formula$mvalue,nrow=nrow(newdata),byrow=TRUE)
-    nx <- ncol(newdata)
-    #getl <- function(times,data=R,ratetable=formula$ratetable){
-    #	-log(srvxp.fit(data,times,ratetable))
-    #}
-    #Lambdap <- sapply(formula$times, getl)
-   # Lambdap <- NULL
-   # for(it in 1:nt){
-   # 	Lambdap <- cbind(Lambdap,-log(srvxp.fit(R,formula$times[it],formula$ratetable)))
-   # }
-   R <- data.frame(R)
-   names(R) <- rat
-    ebx <- exp(as.matrix(newdata)%*%formula$coef)
+    newdata <- newdata[,1:nvar,drop=FALSE]
+    if(any(formula$mvalue)>0)newdata <- newdata - matrix(formula$mvalue,nrow=nx,byrow=TRUE)
+    R <- data.frame(R)
+    names(R) <- rat
+    ebx <- exp(data.matrix(newdata)%*%as.vector(formula$coef))
     ebx <- matrix(ebx,ncol=nt,nrow=length(ebx))
     Lambdae <- Lambda0*ebx
     temp$surv <- t(exp(-Lambdae))
